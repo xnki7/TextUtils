405237b4b8a5f764ad58f3ed8394a9304cbde5f2
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-28 23:53:07 +0530

    Add files via upload

---
+++ src/components/TextForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TextForm(props) {
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" id="myBox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary">Convert to Uppercase</button>
+    </div>
+  );
+}
+
+export default TextForm;
