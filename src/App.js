1659d6537ef1a8de2d11619b1ade5bbf68f8db9a
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2022-12-28 23:51:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" about="About_TextUtils" />
+
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
